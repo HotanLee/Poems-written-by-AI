5e68be22560affb7a7ad4d06d7e2f59a7fe375ad
HotanLee <46061122+HotanLee@users.noreply.github.com>
2020-03-20 11:17:01 +0800

    Update emoi_model.py

--- 图像模型/emotion/emoi_model.py
+++ 图像模型/emotion/emoi_model.py
@@ -6,7 +6,7 @@ from img_preprocess import *
 model = tf.keras.models.load_model('./EMOI_Pre.h5')
 name2table = ['消极', '中性', '积极']
 
-path = 'G:/dataset/GAPED_2/test/P002.jpg'
+path = ''
 x = img_preprocess(path)
 prediction = model.predict(x)
 print(prediction)
